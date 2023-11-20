3ce790e3218b97156e1345cfe87ab81977671f34
Matt Sampson <matt.sampson@princeton.edu>
2023-11-20 12:02:19 -0500

    Update ramses_tar_loader.py

--- ramses_tar_loader.py
+++ ramses_tar_loader.py
@@ -23,6 +23,6 @@ def RAMSES_tar_loader(path, output, extension=".tar.gz"):
     loadName = filenum + "/info_" + f'{output:05d}'
     with tarfile.open(path + filenum + extension, "r") as tf:
         tf.extractall(path="./tmp_dir")    
-    ds = yt.load("./tmp_dir" + loadName)   # load in the file to yt
+    ds = yt.load("./tmp_dir/" + loadName)   # load in the file to yt
     os.rmdir("./tmp_dir")                  # delete temporary extraction
     return ds
